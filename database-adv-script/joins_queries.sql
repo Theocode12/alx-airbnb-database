5402216e9cd19b0d7a947b9b91b826aaea46d565
Theocode12 <vchidubem9@gmail.com>
2024-12-01 21:00:05 +0100

    update joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,7 +1,15 @@
 SELECT * FROM user INNER JOIN booking ON user.user_id = booking.user_id;
 
-SELECT * FROM property LEFT JOIN review ON property.property_id = review.property_id;
+SELECT * FROM property 
+LEFT JOIN review 
+ON property.property_id = review.property_id
+ORDER BY review.rating DESC;
 
 SELECT * FROM user LEFT JOIN booking ON user.user_id = booking.user_id
 UNION
 SELECT * FROM user RIGHT JOIN booking ON user.user_id = booking.user_id;
+
+SELECT * FROM property 
+FULL OUTER JOIN review 
+ON property.property_id = review.property_id
+ORDER BY review.rating DESC;
